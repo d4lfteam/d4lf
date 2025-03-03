87a531f783da09a5e641d0932d2540d4994a4140
CJ Shrader <cjshrader@gmail.com>
2025-03-03 18:11:31 -0500

    Fixed issue where a warning for advanced tooltip information was incorrectly being shown when your inventory was all rares (#458)

--- src/scripts/loot_filter_tts.py
+++ src/scripts/loot_filter_tts.py
@@ -7,7 +7,7 @@ from src.config.loader import IniConfigLoader
 from src.config.models import ItemRefreshType, UnfilteredUniquesType
 from src.item.data.affix import AffixType
 from src.item.data.item_type import ItemType
-from src.item.data.rarity import ItemRarity
+from src.item.data.rarity import ItemRarity, is_junk_rarity
 from src.item.filter import Filter
 from src.scripts.common import is_ignored_item, mark_as_favorite, mark_as_junk, reset_item_status
 from src.ui.inventory_base import InventoryBase
@@ -58,9 +58,10 @@ def check_items(inv: InventoryBase, force_refresh: ItemRefreshType):
         if is_ignored_item(item_descr):
             continue
 
-        num_of_affixed_items_checked += 1
-        if all(affix.type == AffixType.greater for affix in item_descr.affixes):
-            num_of_items_with_all_ga += 1
+        if not is_junk_rarity(item_descr.rarity):
+            num_of_affixed_items_checked += 1
+            if item_descr.affixes and all(affix.type == AffixType.greater for affix in item_descr.affixes):
+                num_of_items_with_all_ga += 1
 
         # Check if we want to keep the item
         res = Filter().should_keep(item_descr)
@@ -103,7 +104,7 @@ def check_items(inv: InventoryBase, force_refresh: ItemRefreshType):
     LOGGER.debug(f"  Time to filter all items in stash/inventory tab: {time.time() - start_checking_items:.2f}s")
 
     # If more than 80% of the items had all greater affixes that means something is probably wrong
-    if num_of_affixed_items_checked > 1 and (num_of_items_with_all_ga / num_of_affixed_items_checked > 0.8):
+    if num_of_affixed_items_checked > 2 and (num_of_items_with_all_ga / num_of_affixed_items_checked > 0.8):
         LOGGER.warning(
-            f"{num_of_items_with_all_ga} out of {num_of_affixed_items_checked} items checked had all greater affixes. You are either exceptionally lucky or have not enabled Advanced Tooltip Information in Options > Gameplay"
+            f"{num_of_items_with_all_ga} out of {num_of_affixed_items_checked} non-junk rarity items checked had all greater affixes. You are either exceptionally lucky or have not enabled Advanced Tooltip Information in Options > Gameplay"
         )
