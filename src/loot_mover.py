1bda8c96196f5f4d8ee0856915a4bc81a08c16ac
CJ Shrader <cjshrader@gmail.com>
2025-05-18 21:13:32 -0400

    Vision Mode with Highlighting will now check shops as well, which is mostly only useful for the shop in The Den. This juice was not worth the squeeze. (#495)

--- src/loot_mover.py
+++ src/loot_mover.py
@@ -4,8 +4,8 @@ from src.cam import Cam
 from src.config.loader import IniConfigLoader
 from src.config.models import MoveItemsType
 from src.ui.char_inventory import CharInventory
-from src.ui.chest import Chest
 from src.ui.inventory_base import ItemSlot
+from src.ui.stash import Stash
 from src.utils.custom_mouse import mouse
 
 LOGGER = logging.getLogger(__name__)
@@ -15,9 +15,9 @@ def move_items_to_stash():
     LOGGER.info("Move inventory items to stash")
 
     inv = CharInventory()
-    chest = Chest()
+    stash = Stash()
 
-    if not chest.is_open():
+    if not stash.is_open():
         LOGGER.error("Moving items only works if stash is open")
         return
 
@@ -28,9 +28,9 @@ def move_items_to_stash():
         return
 
     for i in IniConfigLoader().general.check_chest_tabs:
-        chest.switch_to_tab(i)
+        stash.switch_to_tab(i)
 
-        _, empty_chest = chest.get_item_slots()
+        _, empty_chest = stash.get_item_slots()
 
         if not empty_chest:
             continue
@@ -48,9 +48,9 @@ def move_items_to_inventory():
     LOGGER.info("Move stash items to inventory")
 
     inv = CharInventory()
-    chest = Chest()
+    stash = Stash()
 
-    if not chest.is_open():
+    if not stash.is_open():
         LOGGER.error("Moving items only works if stash is open")
         return
 
@@ -62,8 +62,8 @@ def move_items_to_inventory():
         return
 
     for i in IniConfigLoader().general.check_chest_tabs:
-        chest.switch_to_tab(i)
-        unhandled_slots, _ = chest.get_item_slots()
+        stash.switch_to_tab(i)
+        unhandled_slots, _ = stash.get_item_slots()
 
         LOGGER.debug(
             f"Stash tab {i} - Number of stash items: {len(unhandled_slots)} - Number of empty inventory spaces: {empty_slot_count}"
--- tests/ui/chest_test.py
+++ tests/ui/chest_test.py
@@ -3,7 +3,7 @@ import pytest
 
 from src.cam import Cam
 from src.config import BASE_DIR
-from src.ui.chest import Chest
+from src.ui.stash import Stash
 
 BASE_PATH = BASE_DIR / "tests/assets/ui"
 
@@ -12,6 +12,6 @@ BASE_PATH = BASE_DIR / "tests/assets/ui"
 def test_chest(img_res, input_img):
     Cam().update_window_pos(0, 0, *img_res)
     img = cv2.imread(input_img)
-    inv = Chest()
+    inv = Stash()
     flag = inv.is_open(img)
     assert flag
