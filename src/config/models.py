970ebd5263690ba49001d5d861d94c46f82199d2
chrisoro <4160557+chrisoro@users.noreply.github.com>
2025-12-14 21:38:52 +0100

    Support 7 tabs (#587)

--- src/config/models.py
+++ src/config/models.py
@@ -249,7 +249,7 @@ class GeneralModel(_IniBaseModel):
         description="When using the loot filter, should found temper manuals be automatically used? Note: Will not work with stash open.",
     )
     browser: BrowserType = Field(default=BrowserType.chrome, description="Which browser to use to get builds")
-    check_chest_tabs: list[int] = Field(default=[0, 1], description="Which stash tabs to check. Note: All 6 Tabs must be unlocked!")
+    check_chest_tabs: list[int] = Field(default=[0, 1], description="Which stash tabs to check. Note: All 7 Tabs must be unlocked!")
     full_dump: bool = Field(
         default=False,
         description="When using the import build feature, whether to use the full dump (e.g. contains all filter items) or not",
@@ -590,5 +590,5 @@ class UiRoiModel(NumpyModel):
     slots_5x10: np_array_pydantic_annotated_typing(dimensions=1)
     sort_icon: np_array_pydantic_annotated_typing(dimensions=1)
     stash_menu_icon: np_array_pydantic_annotated_typing(dimensions=1)
-    tab_slots_6: np_array_pydantic_annotated_typing(dimensions=1)
+    tab_slots: np_array_pydantic_annotated_typing(dimensions=1)
     vendor_menu_icon: np_array_pydantic_annotated_typing(dimensions=1)
