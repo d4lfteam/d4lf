6c8dff8a6e908893a6be7e640d2d04b83a8519c9
cjshrader <cjshrader@gmail.com>
2025-12-19 08:34:25 -0500

    (BugFix) Attempted fix for some uniques reporting strange characters in their names

--- src/scripts/__init__.py
+++ src/scripts/__init__.py
@@ -1,4 +1,13 @@
 def correct_name(name: str) -> str | None:
     if name:
-        return name.lower().replace("'", "").replace(" ", "_").replace(",", "").replace("(", "").replace(")", "")
+        return (
+            name.lower()
+            .replace("'", "")
+            .replace(" ", "_")
+            .replace("\xa0", "_")
+            .replace("\u00a0", "_")
+            .replace(",", "")
+            .replace("(", "")
+            .replace(")", "")
+        )
     return name
