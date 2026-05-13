5a341e5b3eebd00915a172b0ce524dbd98f2ce68
chrizzocb <chrizzo.cb@gmail.com>
2026-05-13 22:00:07 +0200

    (S13) Profile editor now supports the new unique model. You can set specific uniques to filter for in the Affixes tab and global unique settings in the GlobalUniques tab (#706)

--- src/gui/profile_editor/uniques_tab.py
+++ src/gui/profile_editor/uniques_tab.py
@@ -1,17 +1,10 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (
-    QCheckBox,
-    QComboBox,
-    QCompleter,
     QDialog,
     QFormLayout,
     QFrame,
     QGroupBox,
-    QHBoxLayout,
     QLineEdit,
-    QListWidget,
-    QListWidgetItem,
-    QMessageBox,
     QPushButton,
     QScrollArea,
     QTabWidget,
@@ -21,19 +14,10 @@ from PyQt6.QtWidgets import (
     QWidget,
 )
 
-from src.config.profile_models import AffixFilterModel, AspectUniqueFilterModel, ComparisonType, GlobalUniqueModel
-from src.dataloader import Dataloader
-from src.gui.dialog import (
-    DeleteItem,
-    IgnoreScrollWheelComboBox,
-    IgnoreScrollWheelSpinBox,
-    MinGreaterDialog,
-    MinPowerDialog,
-)
-from src.gui.profile_editor.affixes_tab import AffixWidget
-from src.item.data.item_type import ItemType, is_armor, is_jewelry, is_weapon
+from src.config.profile_models import GlobalUniqueModel
+from src.gui.dialog import DeleteItem, IgnoreScrollWheelSpinBox
 
-UNIQUES_TABNAME = "Uniques"
+UNIQUES_TABNAME = "GlobalUniques"
 
 
 class UniqueWidget(QWidget):
@@ -47,20 +31,13 @@ class UniqueWidget(QWidget):
         scroll_area = QScrollArea(self)
         scroll_area.setWidgetResizable(True)
         scroll_area.setFrameShape(QFrame.Shape.NoFrame)
-        # Content widget that will hold all our existing UI elements
+
         content_widget = QWidget()
         self.content_layout = QVBoxLayout(content_widget)
         self.content_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
 
         self.create_general_groupbox()
 
-        if self.unique_model.aspect:
-            self.create_aspect_groupbox()
-
-        if self.unique_model.affix != []:
-            self.create_affix_groupbox()
-
-        # Set up scroll area
         scroll_area.setWidget(content_widget)
         self.main_layout = QVBoxLayout()
         self.main_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
@@ -68,154 +45,42 @@ class UniqueWidget(QWidget):
         self.setLayout(self.main_layout)
 
     def create_general_groupbox(self):
-        # General Settings
         self.general_groupbox = QGroupBox()
-        self.general_groupbox.setTitle("General Infos")
+        self.general_groupbox.setTitle("Global Unique Rule")
         self.general_form = QFormLayout()
 
-        self.create_item_type_combobox()
+        self.profile_alias = QLineEdit()
+        self.profile_alias.setMaximumWidth(300)
+        self.profile_alias.setText(self.unique_model.profileAlias)
+        self.profile_alias.textChanged.connect(self.update_profile_alias)
+        self.general_form.addRow("Profile Alias:", self.profile_alias)
 
         self.min_power = IgnoreScrollWheelSpinBox()
-        self.min_power.setMaximum(800)
+        self.min_power.setRange(0, 800)
         self.min_power.setValue(self.unique_model.minPower)
         self.min_power.setMaximumWidth(150)
         self.min_power.valueChanged.connect(self.update_min_power)
         self.general_form.addRow("Minimum Power:", self.min_power)
 
         self.min_greater = IgnoreScrollWheelSpinBox()
+        self.min_greater.setRange(0, 4)
         self.min_greater.setValue(self.unique_model.minGreaterAffixCount)
         self.min_greater.setMaximumWidth(150)
         self.min_greater.valueChanged.connect(self.update_min_greater_affix)
         self.general_form.addRow("Min Greater Affixes:", self.min_greater)
 
         self.min_percent = IgnoreScrollWheelSpinBox()
+        self.min_percent.setRange(0, 100)
         self.min_percent.setValue(self.unique_model.minPercentOfAspect)
-        self.min_percent.setMaximum(100)
         self.min_percent.setMaximumWidth(150)
         self.min_percent.valueChanged.connect(self.update_min_percent)
         self.general_form.addRow("Min Percent of Aspect:", self.min_percent)
 
-        self.mythic = QCheckBox(" ")
-        self.mythic.checkStateChanged.connect(self.update_mythic)
-        self.general_form.addRow("Mythic:", self.mythic)
         self.general_groupbox.setLayout(self.general_form)
         self.content_layout.addWidget(self.general_groupbox)
 
-    def create_item_type_combobox(self):
-        self.item_type_combo = IgnoreScrollWheelComboBox()
-        self.item_type_combo.setEditable(True)
-        self.item_type_combo.setInsertPolicy(QComboBox.InsertPolicy.NoInsert)
-        self.item_type_combo.completer().setCompletionMode(QCompleter.CompletionMode.PopupCompletion)
-        item_types_names = [
-            item.name for item in ItemType.__members__.values() if is_armor(item) or is_jewelry(item) or is_weapon(item)
-        ]
-        item_types_names.append("None")
-        self.item_type_combo.addItems(item_types_names)
-        if len(self.unique_model.itemType) == 0:
-            self.item_type_combo.setCurrentText("None")
-        else:
-            self.item_type_combo.setCurrentText(self.unique_model.itemType[0].name)
-        self.item_type_combo.setMaximumWidth(150)
-        self.item_type_combo.currentIndexChanged.connect(self.update_item_type)
-        self.general_form.addRow("Item Type:", self.item_type_combo)
-
-    def create_aspect_groupbox(self):
-        # Aspect settings
-        self.unique_aspect_groupbox = QGroupBox()
-        self.unique_aspect_groupbox.setTitle("Aspect")
-        self.unique_aspect_form = QFormLayout()
-        self.unique_aspect_groupbox.setMinimumWidth(900)
-
-        self.aspect_name_combo = IgnoreScrollWheelComboBox()
-        self.aspect_name_combo.setEditable(True)
-
-        self.aspect_name_combo.setInsertPolicy(QComboBox.InsertPolicy.NoInsert)
-        self.aspect_name_combo.completer().setCompletionMode(QCompleter.CompletionMode.PopupCompletion)
-        self.aspect_name_combo.addItems(sorted(Dataloader().aspect_unique_dict.keys()))
-        self.aspect_name_combo.setCurrentText(self.unique_model.aspect.name)
-        self.aspect_name_combo.setSizeAdjustPolicy(QComboBox.SizeAdjustPolicy.AdjustToContents)
-        self.aspect_name_combo.setMinimumWidth(600)
-        self.aspect_name_combo.currentIndexChanged.connect(self.update_aspect_name)
-        self.unique_aspect_form.addRow("Name:", self.aspect_name_combo)
-
-        # Value Input
-        self.aspect_value_edit = QLineEdit()
-        self.aspect_value_edit.setMaximumWidth(100)
-        self.aspect_value_edit.setPlaceholderText("Value (optional)")
-        if self.unique_model.aspect.value is not None:
-            self.aspect_value_edit.setText(str(self.unique_model.aspect.value))
-        self.aspect_value_edit.textChanged.connect(self.update_aspect_value)
-        self.unique_model.aspect.value = self.unique_model.aspect.value
-        self.unique_aspect_form.addRow("Value:", self.aspect_value_edit)
-        # Comparison Combobox
-        self.comparison_combo = IgnoreScrollWheelComboBox()
-        self.comparison_combo.setEditable(True)
-        self.comparison_combo.setInsertPolicy(QComboBox.InsertPolicy.NoInsert)
-        self.comparison_combo.completer().setCompletionMode(QCompleter.CompletionMode.PopupCompletion)
-        self.comparison_combo.setMaximumWidth(100)
-        self.comparison_combo.addItems([ct.value for ct in ComparisonType])
-        self.comparison_combo.setCurrentText(self.unique_model.aspect.comparison.value)
-        self.comparison_combo.currentIndexChanged.connect(self.update_aspect_comparison)
-        self.unique_model.aspect.comparison = ComparisonType(self.unique_model.aspect.comparison.value)
-        self.unique_aspect_form.addRow("Comparison:", self.comparison_combo)
-        self.unique_aspect_groupbox.setLayout(self.unique_aspect_form)
-        self.content_layout.insertWidget(1, self.unique_aspect_groupbox)
-        # self.content_layout.addWidget(self.unique_aspect_groupbox)
-
-    def remove_aspect_groupbox(self):
-        self.unique_aspect_groupbox.setParent(None)
-
-    def create_affix_groupbox(self):
-        self.affix_groupbox = QGroupBox()
-        self.affix_groupbox.setTitle("Affixes")
-        self.affix_groupbox_layout = QVBoxLayout()
-
-        self.affix_list = QListWidget()
-        self.affix_list.setMinimumHeight(200)
-        self.affix_list.setAlternatingRowColors(True)
-        for affix in self.unique_model.affix:
-            self.add_affix_item(affix)
-
-        affix_btn_layout = QHBoxLayout()
-        add_affix_btn = QPushButton("Add Affix")
-        add_affix_btn.clicked.connect(self.add_affix)
-        affix_btn_layout.addWidget(add_affix_btn)
-        remove_affix_btn = QPushButton("Remove Affix")
-        remove_affix_btn.clicked.connect(self.remove_affix)
-        affix_btn_layout.addWidget(remove_affix_btn)
-        self.affix_groupbox_layout.addLayout(affix_btn_layout)
-        self.affix_groupbox_layout.addWidget(self.affix_list)
-        self.affix_groupbox.setLayout(self.affix_groupbox_layout)
-        self.content_layout.addWidget(self.affix_groupbox)
-
-    def remove_affix_groupbox(self):
-        self.affix_groupbox.setParent(None)
-
-    def add_affix_item(self, affix: AffixFilterModel):
-        item = QListWidgetItem()
-        widget = AffixWidget(affix)
-        item.setSizeHint(widget.sizeHint())
-        self.affix_list.addItem(item)
-        self.affix_list.setItemWidget(item, widget)
-
-    def add_affix(self):
-        new_affix = AffixFilterModel(
-            name=next(iter(Dataloader().affix_dict.keys())), value=None, comparison=ComparisonType.larger
-        )
-        self.unique_model.affix.append(new_affix)
-        self.add_affix_item(new_affix)
-
-    def remove_affix(self):
-        for item in self.affix_list.selectedItems():
-            row = self.affix_list.row(item)
-            self.affix_list.takeItem(row)
-            del self.unique_model.affix[row]
-
-    def update_item_type(self):
-        if self.item_type_combo.currentText() == "None":
-            self.unique_model.itemType = []
-        else:
-            self.unique_model.itemType = [ItemType(ItemType._member_map_[self.item_type_combo.currentText()])]
+    def update_profile_alias(self, value: str):
+        self.unique_model.profileAlias = value.strip()
 
     def update_min_power(self):
         self.unique_model.minPower = self.min_power.value()
@@ -225,22 +90,6 @@ class UniqueWidget(QWidget):
 
     def update_min_percent(self):
         self.unique_model.minPercentOfAspect = self.min_percent.value()
-
-    def update_mythic(self):
-        self.unique_model.mythic = self.mythic.isChecked()
-
-    def update_aspect_name(self):
-        self.unique_model.aspect.name = self.aspect_name_combo.currentText()
-
-    def update_aspect_value(self, value):
-        try:
-            self.unique_model.aspect.value = float(value) if value else None
-        except ValueError:
-            return
-
-    def update_aspect_comparison(self):
-        comparison = self.comparison_combo.currentText()
-        self.unique_model.aspect.comparison = ComparisonType(comparison)
 
 
 class UniquesTab(QWidget):
@@ -272,32 +121,14 @@ class UniquesTab(QWidget):
         self.toolbar.setMovable(False)
         for i, unique_model in enumerate(self.unique_model_list):
             group = UniqueWidget(unique_model)
-            self.tab_widget.addTab(group, f"Unique {i}")
-        # Add buttons to toolbar
-        add_item_button = QPushButton("Create Unique")
-        remove_item_button = QPushButton("Remove Unique")
-        add_aspect_button = QPushButton("Add Aspect to current unique")
-        remove_aspect_button = QPushButton("Remove Aspect to current unique")
-        add_affixes_button = QPushButton("Add Affixes to current unique")
-        remove_affixes_button = QPushButton("Remove Affixes to current unique")
-        set_all_minGreaterAffix_button = QPushButton("Set all minGreaterAffix")
-        set_all_minPower_button = QPushButton("Set all minPower")
+            self.tab_widget.addTab(group, f"Unique Rule {i}")
+
+        add_item_button = QPushButton("Create Rule")
+        remove_item_button = QPushButton("Remove Rule")
         add_item_button.clicked.connect(self.add_item_type)
         remove_item_button.clicked.connect(self.remove_item_type)
-        add_aspect_button.clicked.connect(self.add_aspect_to_current_unique)
-        add_affixes_button.clicked.connect(self.add_affixes_to_current_unique)
-        remove_aspect_button.clicked.connect(self.remove_aspect_from_current_unique)
-        remove_affixes_button.clicked.connect(self.remove_affixes_from_current_unique)
-        set_all_minGreaterAffix_button.clicked.connect(self.set_all_minGreaterAffix)
-        set_all_minPower_button.clicked.connect(self.set_all_minPower)
         self.toolbar.addWidget(add_item_button)
         self.toolbar.addWidget(remove_item_button)
-        self.toolbar.addWidget(add_aspect_button)
-        self.toolbar.addWidget(remove_aspect_button)
-        self.toolbar.addWidget(add_affixes_button)
-        self.toolbar.addWidget(remove_affixes_button)
-        self.toolbar.addWidget(set_all_minGreaterAffix_button)
-        self.toolbar.addWidget(set_all_minPower_button)
         self.main_layout.addWidget(self.toolbar)
         self.main_layout.addWidget(self.tab_widget)
 
@@ -322,66 +153,10 @@ class UniquesTab(QWidget):
 
     def rename_tabs(self):
         for i in range(self.tab_widget.count()):
-            self.tab_widget.setTabText(i, f"Unique {i}")
+            self.tab_widget.setTabText(i, f"Unique Rule {i}")
 
     def add_item_type(self):
         unique_model = GlobalUniqueModel()
         group = UniqueWidget(unique_model)
-        self.tab_widget.addTab(group, f"Unique {self.tab_widget.count()}")
+        self.tab_widget.addTab(group, f"Unique Rule {self.tab_widget.count()}")
         self.unique_model_list.append(unique_model)
-
-    def add_aspect_to_current_unique(self):
-        current_unique: UniqueWidget = self.tab_widget.currentWidget()
-        if current_unique.unique_model.aspect:
-            QMessageBox.warning(
-                self, "Warn", "An aspect already exist for the current unique. Please modify the existing one."
-            )
-        else:
-            current_unique.unique_model.aspect = AspectUniqueFilterModel(
-                name=min(Dataloader().aspect_unique_dict.keys())
-            )
-            current_unique.create_aspect_groupbox()
-
-    def add_affixes_to_current_unique(self):
-        current_unique: UniqueWidget = self.tab_widget.currentWidget()
-        if current_unique.unique_model.affix:
-            QMessageBox.warning(
-                self, "Warn", "An affix already exist for the current unique. Please modify the existing one."
-            )
-        else:
-            current_unique.unique_model.affix = [AffixFilterModel(name=min(Dataloader().affix_dict.keys()))]
-            current_unique.create_affix_groupbox()
-
-    def remove_aspect_from_current_unique(self):
-        current_unique: UniqueWidget = self.tab_widget.currentWidget()
-        if not current_unique.unique_model.aspect:
-            QMessageBox.warning(self, "Warn", "There is no aspect in the current unique. You can add one.")
-        else:
-            current_unique.unique_model.aspect = None
-            current_unique.remove_aspect_groupbox()
-
-    def remove_affixes_from_current_unique(self):
-        current_unique: UniqueWidget = self.tab_widget.currentWidget()
-        if not current_unique.unique_model.affix:
-            QMessageBox.warning(self, "Warn", "There is no affix in the current unique. You can add one.")
-        else:
-            current_unique.unique_model.affix = []
-            current_unique.remove_affix_groupbox()
-
-    def set_all_minGreaterAffix(self):
-        dialog = MinGreaterDialog(self)
-        if dialog.exec() == QDialog.DialogCode.Accepted:
-            minGreaterAffix = dialog.get_value()
-            for i in range(self.tab_widget.count()):
-                tab: UniqueWidget = self.tab_widget.widget(i)
-                tab.min_greater.setValue(minGreaterAffix)
-                tab.update_min_greater_affix()
-
-    def set_all_minPower(self):
-        dialog = MinPowerDialog(self)
-        if dialog.exec() == QDialog.DialogCode.Accepted:
-            minPower = dialog.get_value()
-            for i in range(self.tab_widget.count()):
-                tab: UniqueWidget = self.tab_widget.widget(i)
-                tab.min_power.setValue(minPower)
-                tab.update_min_power()
