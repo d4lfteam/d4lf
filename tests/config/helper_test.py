fd8c6579b8e33a03ef986b3f6f3a1d25cbef7c49
cjshrader <cjshrader@gmail.com>
2026-05-21 11:35:41 -0400

    Revert "(New Feature) You can now disable a hotkey if you want it completely turned off. (#737)"
    This reverts commit f82c6be1d23c88a5eb6bdde69522f7b1297d05dd.

--- tests/config/helper_test.py
+++ tests/config/helper_test.py
@@ -1,7 +1,6 @@
 import pytest
 
-from src.config.helper import singleton, str_to_int_list, to_keyboard_hotkey, validate_hotkey
-from src.config.settings_models import AdvancedOptionsModel
+from src.config.helper import singleton, str_to_int_list, validate_hotkey
 
 
 class TestKeyMustExist:
@@ -12,42 +11,10 @@ class TestKeyMustExist:
     def test_modifier_key_works(self):
         assert validate_hotkey("shift+a")
 
-    @pytest.mark.parametrize("hotkey", ["left", "right", "up", "down", "shift+left", "ctrl+right", "alt+up"])
-    def test_arrow_keys_work(self, hotkey):
-        assert validate_hotkey(hotkey, allow_empty=True)
-
-    def test_layout_sensitive_key_uses_scan_code_for_keyboard_hook(self, monkeypatch):
-        umlaut_key = "ctrl+\N{LATIN SMALL LETTER O WITH DIAERESIS}"
-
-        def fake_parse_hotkey(hotkey):
-            return {umlaut_key: (((29, 57373), (39,)),), "^": (((41, -255),),)}[hotkey]
-
-        monkeypatch.setattr("src.config.helper.keyboard.parse_hotkey", fake_parse_hotkey)
-
-        assert to_keyboard_hotkey(umlaut_key) == (((29, 57373), (39,)),)
-        assert to_keyboard_hotkey("^") == (((41,),),)
-
-    def test_empty_key_disables_optional_hotkey(self):
-        assert not validate_hotkey("", allow_empty=True)
-
-    def test_empty_key_fails_by_default(self):
-        with pytest.raises(ValueError, match="Can only normalize non-empty string names."):
-            validate_hotkey("")
-
     def test_non_existing_key(self):
         # Test for a non-existing key
         with pytest.raises(ValueError, match="Key 'non_existing_key' is not mapped to any known key."):
             validate_hotkey("non_existing_key")
-
-    def test_empty_keys_are_excluded_from_unique_hotkey_check(self):
-        model = AdvancedOptionsModel(exit_key="", toggle_paragon_overlay="")
-
-        assert not model.exit_key
-        assert not model.toggle_paragon_overlay
-
-    def test_non_empty_keys_must_still_be_unique(self):
-        with pytest.raises(ValueError, match="hotkeys must be unique"):
-            AdvancedOptionsModel(exit_key="f10", toggle_paragon_overlay="f10")
 
 
 class TestSingletonDecorator:
