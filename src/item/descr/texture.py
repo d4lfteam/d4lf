d9e8062fd6adea935afae949f9e5852be4253ba6
cjshrader <cjshrader@gmail.com>
2026-07-03 16:36:40 -0400

    (BugFix) Fixed issue reading legendary seals when at 1080p

--- src/item/descr/texture.py
+++ src/item/descr/texture.py
@@ -108,6 +108,7 @@ def find_affix_bullets(img_item_descr: np.ndarray, sep_short_match: TemplateMatc
             "greater_affix_bullet_point_1080p_special",
             "greater_affix_bullet_point_masterworked_medium_1080p_special",
             "masterworking_affix_bullet_medium_1080p_special",
+            "seal_set_bullet_point_1080p_special",
         ]
         # At lower resolutions it starts reading text as bullet points. We'll see if this fixes it
         search_threshold = 0.85
